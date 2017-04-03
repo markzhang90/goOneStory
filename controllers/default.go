a5de2f8e75d77c5ff22324ae24be4711338b187a
Yiyuan Zhang <markzhang90@gmail.com>
2017-04-03 13:40:40 +0800

    cooool

--- controllers/default.go
+++ controllers/default.go
@@ -8,8 +8,20 @@ type MainController struct {
 	beego.Controller
 }
 
+
 func (c *MainController) Get() {
 	c.Data["Website"] = "beego.me"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
 }
+
+type HelloController struct {
+	beego.Controller
+}
+
+func (c *HelloController) Get() {
+	varId := c.Ctx.Input.Param(":id")
+	varTest := c.Ctx.Input.Param(":test")
+	c.Ctx.WriteString("hello world" + varId + varTest)
+}
+
